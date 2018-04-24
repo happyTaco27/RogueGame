7b11617d294c29ac1a908edb6287645a65c4324a
happyTaco27 <36053877+happyTaco27@users.noreply.github.com>
2018-04-24 15:14:41 -0600

    making grid work

--- RogueGame/sketch.js
+++ RogueGame/sketch.js
@@ -1,55 +1,80 @@
 let characterXPos;
 let characterYPos;
-let map;
 let interact;
 let cols,rows,size,grid;
-
+size=100;
+let theMap=[
+  [0,0,0,0,0,0],
+  [0,1,1,1,1,0],
+  [0,1,1,1,1,0],
+  [2,3,1,1,1,0],
+  [4,2,3,1,1,0],
+  [4,4,2,0,0,0],
+];
 
 function setup() {
   createCanvas(windowWidth,windowHeight);
-  map= new TheGrid();
 }
-class TheGrid {
-  //making the map
-  constructor() {
-    this.cols=8;
-    this.rows=8;
-    if(width<height){
-      this.size=width/rows;
-    }
-    else{
-      this.size=height/cols;
-    }
-    this.grid;
-    this.posX;
-    this.posy;
-  }
-  emptyGrid(cols,rows){
-    let mapGrid = [];
-    for (let x=0; x<cols; x++) {
-      mapGrid.push([]);
-      for (let y=0; y<rows; y++) {
-        mapGrid[x].push(0);
+function map()  {
+  for(let i=0;i<5;i++){
+    for(let j=0;j<5;j++){
+      if(theMap[i][j]===0){
+        fill(0);
+        rect(i*size,j*size,size,size);
       }
-    }
-    return mapGrid;
-  }
-  display(){
-    for (let x=0; x<cols; x++) {
-      for (let y=0; y<rows; y++) {
-        rect(x*size, y*size, size, size);
+      else{
+        fill(255);
+        rect(i*size,j*size,size,size);
       }
     }
   }
+
 }
-function keyPressed(){
-  if (key === "m" || key === "M"){
-    map = true;
-  }
-  else if (key === "f" || key === "F"){
-    interact = true;
-  }
+function draw(){
+  background(255);
+  map();
 }
+// class TheGrid {
+//   //making the map
+//   constructor() {
+//     this.cols=8;
+//     this.rows=8;
+//     if(width<height){
+//       this.size=width/rows;
+//     }
+//     else{
+//       this.size=height/cols;
+//     }
+//     this.grid;
+//     this.posX;
+//     this.posy;
+//   }
+//   emptyGrid(cols,rows){
+//     let mapGrid = [];
+//     for (let x=0; x<cols; x++) {
+//       mapGrid.push([]);
+//       for (let y=0; y<rows; y++) {
+//         mapGrid[x].push(0);
+//       }
+//     }
+//     return mapGrid;
+//   }
+//   display(){
+//     for (let x=0; x<cols; x++) {
+//       for (let y=0; y<rows; y++) {
+//         rect(x*size, y*size, size, size);
+//       }
+//     }
+//   }
+// }
+// function keyPressed(){
+//   if (key === "m" || key === "M"){
+//     map = true;
+//   }
+//   else if (key === "f" || key === "F"){
+//     interact = true;
+//   }
+// }
 // class Player {
 //   constructor(x,y) {
 //     this.x;
